dd7cab5a1e2928a4848f43a01562f687bbbe9f20
Vik-tor39 <victor.suquilanda@udla.edu.ec>
2024-10-23 05:38:48 -0500

    Commit3: Se modificaron algunas etiquetas para definir el estilo de la página

--- VictorSuquilanda-TallerClase1/VictorSuquilanda-TallerClase1/obj/Debug/net8.0/VictorSuquilanda-TallerClase1.AssemblyInfo.cs
+++ VictorSuquilanda-TallerClase1/VictorSuquilanda-TallerClase1/obj/Debug/net8.0/VictorSuquilanda-TallerClase1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VictorSuquilanda-TallerClase1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+563f8a09ec31d5951dbc2e302382e556770ade14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c1777aad39935bdf0f20127ea83257ccf7fd77f")]
 [assembly: System.Reflection.AssemblyProductAttribute("VictorSuquilanda-TallerClase1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VictorSuquilanda-TallerClase1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
