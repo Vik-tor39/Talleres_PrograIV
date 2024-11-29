47caef5b2d9e223e87a39f8b185f04ea173e4933
Vik-tor39 <victor.suquilanda@udla.edu.ec>
2024-11-29 03:17:03 -0500

    Commit 1: Se definio un Flayout y un TabBar para navegar entre las página del proyecto

---
+++ MauiPages/MauiPages/Page1.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiPages;
+
+public partial class Page1 : ContentPage
+{
+	public Page1()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiPages/MauiPages/Page3.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiPages;
+
+public partial class Page3 : ContentPage
+{
+	public Page3()
+	{
+		InitializeComponent();
+	}
+}
